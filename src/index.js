651057ff887a7f01ddf91c27490cc3bbd485b427
avbhardwaj <avbhardwaj@live.com>
2018-06-06 11:43:58 -0700

    Removed Unnecessary Code

--- src/index.js
+++ src/index.js
@@ -2,9 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import  './index.css'
-// import API_KEY from './vis-components/API';
-// import YTSearch from 'youtube-api-search';
-//import SearchBar from './vis-components/search_bar';
-
 
 ReactDOM.render( < App /> , document.getElementById('root'));
